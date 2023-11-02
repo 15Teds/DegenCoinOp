fbcfa386494d624919f475aad8301f1842cd66c4
15Teds <teddy55johnson@gmail.com>
2023-11-02 10:43:01 -0400

    Change API

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import { Authorization, AuthorizationContext } from "coinopreact/auth.context";
 const App = () => {
 	return (
 		<>
-			<CoinOpProvider GAME_SLUG="DCF" API_URL="https://api.test.com">
+			<CoinOpProvider GAME_SLUG="DCF" API_URL="https://example.com">
+				{/* The API is will not work in this case, you should be given an approved API URL. */}
 				<Demo />
 			</CoinOpProvider>
 		</>
